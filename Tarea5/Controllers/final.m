7da78b14aa217f6d82ffe7c7acd80865648932d9
Cesar Roberto Rodriguez Silva <Cesar@MacBook-Air-de-macbook-air.local>
2017-11-01 19:18:00 -0600

    primer comer

---
+++ Tarea5/Controllers/final.m
@@ -0,0 +1,37 @@
+//
+//  final.m
+//  Tarea5
+//
+//  Created by Cesar Roberto Rodriguez Silva on 01/11/17.
+//  Copyright © 2017 2drink. All rights reserved.
+//
+
+#import "final.h"
+
+@interface final ()
+
+@end
+
+@implementation final
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Tarea5/Controllers/tablecontrollers.h
@@ -0,0 +1,13 @@
+//
+//  tablecontrollers.h
+//  Tarea5
+//
+//  Created by Cesar Roberto Rodriguez Silva on 01/11/17.
+//  Copyright © 2017 2drink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tablecontrollers : UIViewController
+
+@end
